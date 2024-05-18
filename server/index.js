ac5fa40f67a737dd2003a2eaee52ac7eef3c4ae6
Hua Jin <huazjin16@gmail.com>
2024-05-17 20:36:48 -0500

    connect to database and create user model

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 /* CONFIGURATIONS */
 const app = express();
 dotenv.config();
+
+/* DATABASE CONNECTION */
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log("Database not connected", err);
+  });
 
 /* SERVER CONNECTION */
 const PORT = process.env.PORT || 8000;
